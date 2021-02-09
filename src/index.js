0c8eab1e994af82fa112c13775ef526989aad6d2
Diego Caceres <diego.rca.23@gmail.com>
2021-02-09 00:09:00 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const config = require("./server/config");
+
+//DATABASE
+require("./database");
+
+const app = config(express());
+
+
+app.listen(app.get("port"), () => {
+    console.log("Server on port", app.get("port"));
+})
